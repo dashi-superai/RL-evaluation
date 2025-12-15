1e5e2410d05532e17f8963d6dd2874aba07850d9
dashi-superai <denysyudakov@gmail.com>
2025-12-15 15:29:09 -0500

    update

--- setup.sh
+++ setup.sh
@@ -5,7 +5,6 @@ sudo apt install python3-venv
 python3 -m venv .venv
 source .venv/bin/activate
 pip install uv
-uv pip install fastapi uvicorn transformers torch accelerate math_verify datasets
+uv pip install fastapi uvicorn transformers torch accelerate math_verify datasets openai
 cd affinetes
 uv pip install -e .
-uv pip install openai
