ff3339c57812a125fe3babf0eccb88a139ac804f
dashi-superai <denysyudakov@gmail.com>
2025-12-15 14:34:04 -0500

    update the evaluation

--- setup.sh
+++ setup.sh
@@ -1,6 +1,5 @@
 git clone https://github.com/dashi-superai/RL-evaluation.git
 cd RL-evaluation
-git clone https://github.com/AffineFoundation/affinetes.git
 sudo apt install python3-venv
 python3 -m venv .venv
 source .venv/bin/activate
@@ -8,3 +7,5 @@ pip install uv
 uv pip install fastapi uvicorn transformers torch accelerate math_verify datasets
 cd affinetes
 uv pip install -e .
+cd ..
+
