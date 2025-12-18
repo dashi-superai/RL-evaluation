5a284f8c1219a7be531f28daf37bb82e8b233ba0
dashi-superai <denysyudakov@gmail.com>
2025-12-18 18:43:38 -0500

    update

--- setup.sh
+++ setup.sh
@@ -3,7 +3,8 @@ sudo apt install python3-venv
 python3 -m venv .venv
 source .venv/bin/activate
 pip install uv
-uv pip install fastapi uvicorn transformers torch accelerate math_verify datasets openai verify vllm
+uv pip install fastapi uvicorn transformers torch accelerate math_verify datasets openai verify
+uv pip install vllm
 cd affinetes
 uv pip install -e .
 cd ../
