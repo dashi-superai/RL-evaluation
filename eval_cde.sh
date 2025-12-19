703ba6ec14eac27adc32b32354dc59982e150072
dashi-superai <denysyudakov@gmail.com>
2025-12-19 11:52:34 -0500

    update the code environment

--- eval_cde.sh
+++ eval_cde.sh
@@ -1,2 +1,2 @@
 source .venv/bin/activate
-python ./affinetes/environments/primeintellect/cde/env.py --start_idx 0 --idx_step 5
+python ./affinetes/environments/primeintellect/cde/env.py --start_idx 0 --idx_step 5 --hug_url "AIdashi/dashi-2-1"
